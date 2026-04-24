a05908ac96bdec482b2d2177d558242e8814b168
Hakimi Jaff Al Ridwan <nnridwan18@gmail.com>
2026-04-24 10:24:34 +0700

    Upload GitHub Ke2

---
+++ data.js
@@ -0,0 +1,21 @@
+const databaseProyek = {
+    "sistem_pos": {
+        judul: "Keahlian Skylar ",
+        keahlian: ["JavaScript, PHP"],
+        gambar: "https://static.republika.co.id/uploads/member/images/news/medium_Skylar_ea6d0f7be4_1755903607.jpeg",
+        latar: "Skylar adalah pro player MLBB asal Manado yang dikenal sebagai Gold Laner andal. Memulai karier dari turnamen warkop dan sempat berjuang menjadi kuli angkat batu, ia sukses besar bersama RRQ Hoshi selama 5 tahun sebelum pindah ke ONIC pada 2026.",
+        tantangan: "Sulit Untuk Menjadi seperti dia",
+        pelajaran: "Saya mendapatkan pelajaran bahwa Skylar adalah pemain ML terbaik sepanjang masa",
+        link: ""
+    },
+
+"sistem_pos2": {
+        judul: "Keahlian Skylar ",
+        keahlian: ["JavaScript, PHP"],
+        gambar: "https://static.republika.co.id/uploads/member/images/news/medium_Skylar_ea6d0f7be4_1755903607.jpeg",
+        latar: "Skylar adalah pro player MLBB asal Manado yang dikenal sebagai Gold Laner andal. Memulai karier dari turnamen warkop dan sempat berjuang menjadi kuli angkat batu, ia sukses besar bersama RRQ Hoshi selama 5 tahun sebelum pindah ke ONIC pada 2026.",
+        tantangan: "Sulit Untuk Menjadi seperti dia",
+        pelajaran: "Saya mendapatkan pelajaran bahwa Skylar adalah pemain ML terbaik sepanjang masa",
+        link: ""
+    },
+};
