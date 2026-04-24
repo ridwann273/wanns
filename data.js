1df4d655fbfef223ff1c31d22af3650e5232bb30
Hakimi Jaff Al Ridwan <nnridwan18@gmail.com>
2026-04-24 11:06:02 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -1,21 +1,30 @@
 const databaseProyek = {
     "sistem_pos": {
-        judul: "Keahlian Skylar ",
+        judul: "Xiao Yan",
         keahlian: ["JavaScript, PHP"],
-        gambar: "https://static.republika.co.id/uploads/member/images/news/medium_Skylar_ea6d0f7be4_1755903607.jpeg",
-        latar: "Skylar adalah pro player MLBB asal Manado yang dikenal sebagai Gold Laner andal. Memulai karier dari turnamen warkop dan sempat berjuang menjadi kuli angkat batu, ia sukses besar bersama RRQ Hoshi selama 5 tahun sebelum pindah ke ONIC pada 2026.",
-        tantangan: "Sulit Untuk Menjadi seperti dia",
-        pelajaran: "Saya mendapatkan pelajaran bahwa Skylar adalah pemain ML terbaik sepanjang masa",
+        gambar: "https://www.newhanfu.com/wp-content/uploads/2025/10/What-Divine-Chances-Did-Xiao-Yan-Seize-1.jpg",
+        latar: "Xiao Yan adalah protagonis utama dalam seri Battle Through the Heavens (BTTH), putra dari Xiao Zhan dan Gu Wenxin. Ia jenius berbakat yang kekuatannya merosot drastis menjadi \"sampah keluarga\" pada usia 12 tahun setelah mencapai tingkat 9 Dou Qi pada usia 10 tahun. Penurunan kekuatan ini menyebabkan penghinaan, pembatalan pernikahan, dan menjadi titik balik perjuangannya.",
+        tantangan: "mengembalikan kehormatan keluarganya setelah ia mengalami kemunduran bakat, serta membalas dendam atas penderitaan yang ditimpakan pada keluarganya.",
+        pelajaran: "Xiao Yan menunjukkan bahwa kegagalan hanyalah sementara. Meskipun sempat kehilangan kekuatannya dan diremehkan, ia tidak menyerah, terus berlatih, dan berusaha keras untuk bangkit kembali.",
         link: ""
     },
 
 "sistem_pos2": {
-        judul: "Keahlian Skylar ",
+        judul: "Tang San",
         keahlian: ["JavaScript, PHP"],
-        gambar: "https://static.republika.co.id/uploads/member/images/news/medium_Skylar_ea6d0f7be4_1755903607.jpeg",
-        latar: "Skylar adalah pro player MLBB asal Manado yang dikenal sebagai Gold Laner andal. Memulai karier dari turnamen warkop dan sempat berjuang menjadi kuli angkat batu, ia sukses besar bersama RRQ Hoshi selama 5 tahun sebelum pindah ke ONIC pada 2026.",
-        tantangan: "Sulit Untuk Menjadi seperti dia",
-        pelajaran: "Saya mendapatkan pelajaran bahwa Skylar adalah pemain ML terbaik sepanjang masa",
+        gambar: "https://static.wikia.nocookie.net/soulland/images/a/a1/260.jpeg/revision/latest?cb=20230625103925",
+        latar: "Tang San adalah tokoh protagonis utama dalam seri novel fantasi Soul Land (Douluo Dalu) yang ditulis oleh Tang Jia San Shao. Ia dikenal sebagai individu yang tenang, peduli, dan ahli dalam teknik rahasia Sekte Tang. Tang San reinkarnasi dari kehidupan sebelumnya, membangkitkan roh kembar (palu dan rumput), dan menjadi salah satu dewa terkuat.",
+        tantangan: "Salah satu tantangan terbesar bagi Tang San adalah menyelesaikan sembilan ujian dari Dewa Laut di Pulau Dewa Laut. Ujian ini sangat berat dan menguras fisik serta mental, bertujuan untuk menguji kelayakannya mewarisi posisi Dewa Laut.",
+        pelajaran: "Kekuatan Tang San tidak didapat secara instan, melainkan melalui pelatihan intensif sejak kecil, membangkitkan kekuatannya pada usia 6 tahun, dan terus mengasah kemampuannya.",
+        link: ""
+    },
+    "sistem_pos3": {
+        judul: "Wang Lin",
+        keahlian: ["JavaScript, PHP"],
+        gambar: "https://wallpapercave.com/wp/wp14093307.jpg",
+        latar: "Wang Lin adalah protagonis utama dalam novel xianxia (fantasi kultivasi) Tiongkok populer berjudul Renegade Immortal (Xian Ni). Ia dikenal sebagai individu yang cerdas, tekun, dan memiliki bakat luar biasa dalam kultivasi. Wang Lin memulai perjalanannya dari seorang pemuda biasa hingga menjadi sosok yang ditakuti di dunia kultivasi.",
+        tantangan: "Salah satu tantangan terbesar bagi Wang Lin adalah menghadapi berbagai musuh yang kuat dan mengatasi rintangan dalam perjalanannya untuk mencapai puncak kultivasi. Ia harus berjuang melawan kekuatan jahat dan mengungkap rahasia masa lalunya.",
+        pelajaran: "Kekuatan Wang Lin diperoleh melalui kerja keras, ketekunan, dan pengalaman pahit yang membentuknya menjadi sosok yang tangguh. Ia mengajarkan bahwa perjalanan kultivasi adalah proses yang panjang dan penuh liku.",
         link: ""
     },
 };
